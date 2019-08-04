a811cfb371bc0e09d35c89cef2cbdad67c027ba5
Alan Xavier <alan_xvr@hotmail.com>
2019-08-04 15:43:05 -0400

    Refactor PageContent as a functional component

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,17 +1,14 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "./contexts/ThemeContext";
 
-export default class PageContent extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkMode } = this.context;
-    const styles = {
-      backgroundColor: isDarkMode
-        ? "rgba(0, 0, 0, 0.75)"
-        : "rgba(255, 255, 255, 0.5)",
-      height: "100vh",
-      width: "100vw"
-    };
-    return <div style={styles}>{this.props.children}</div>;
-  }
+export default function PageContent(props) {
+  const { isDarkMode } = useContext(ThemeContext);
+  const styles = {
+    backgroundColor: isDarkMode
+      ? "rgba(0, 0, 0, 0.75)"
+      : "rgba(255, 255, 255, 0.5)",
+    height: "100vh",
+    width: "100vw"
+  };
+  return <div style={styles}>{props.children}</div>;
 }
